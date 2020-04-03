004bede61021cd9b021a345cbda40af44e3c2734
Seohyun Lee <04seohyun@gmail.com>
2020-04-03 19:23:47 +0900

    Change Logo Size

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function createWindow() {
     });
     mainWindow.loadURL(getFramePath());
     mainWindow.setMenu(null);
-    mainWindow.webContents.openDevTools();
+    //mainWindow.webContents.openDevTools();
     mainWindow.on('closed', () => {
         mainWindow = null;
     })
